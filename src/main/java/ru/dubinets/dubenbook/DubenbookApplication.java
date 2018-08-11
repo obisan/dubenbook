880dfe1ad619a12cb2232ec4b946c4c22a2fd243
Dubinets Aleksandr <dubinets.av@gmail.com>
2018-08-11 17:18:40 +1000

    init

---
+++ src/main/java/ru/dubinets/dubenbook/DubenbookApplication.java
@@ -0,0 +1,12 @@
+package ru.dubinets.dubenbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubenbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubenbookApplication.class, args);
+    }
+}
